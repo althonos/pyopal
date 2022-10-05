037f924947163f158affdf71caff189bc9647ea8
Martin Larralde <martin.larralde@embl.de>
2022-10-05 14:16:19 +0200

    Add basic test using the `test1` sequences from the Opal test data

--- pyopal/tests/__init__.py
+++ pyopal/tests/__init__.py
@@ -2,9 +2,10 @@
 
 from . import (
     test_doctest,
+    test_search,
 )
-
 
 def load_tests(loader, suite, pattern):
     suite.addTests(loader.loadTestsFromModule(test_doctest))
+    suite.addTests(loader.loadTestsFromModule(test_search))
     return suite
---
+++ pyopal/tests/test_search.py
@@ -0,0 +1,53 @@
+import unittest
+
+import pyopal
+
+
+
+class TestSearch(unittest.TestCase):
+
+    def test_test1(self):
+
+        query = "ACCTCG"
+        target = "AACCGCTG"
+
+        db = pyopal.Database([target])
+        
+        results = db.search(query)
+        self.assertEqual(len(db), 1)
+        self.assertEqual(results[0].score, 47)
+        self.assertIs(results[0].alignment, None)
+        self.assertIs(results[0].query_start, None)
+        self.assertIs(results[0].query_end, None)
+        self.assertIs(results[0].target_start, None)
+        self.assertIs(results[0].target_end, None)
+
+        results = db.search(query)
+        self.assertEqual(len(db), 1)
+        self.assertEqual(results[0].score, 47)
+        self.assertIs(results[0].alignment, None)
+        self.assertIs(results[0].query_start, None)
+        self.assertIs(results[0].query_end, None)
+        self.assertIs(results[0].target_start, None)
+        self.assertIs(results[0].target_end, None)
+
+        results = db.search(query, mode="end")
+        self.assertEqual(len(db), 1)
+        self.assertEqual(results[0].score, 47)
+        self.assertIs(results[0].alignment, None)
+        self.assertIs(results[0].query_start, None)
+        self.assertEqual(results[0].query_end, 5)
+        self.assertIs(results[0].target_start, None)
+        self.assertEqual(results[0].target_end, 7)
+
+        results = db.search(query, mode="full")
+        self.assertEqual(len(db), 1)
+        self.assertEqual(results[0].score, 47)
+        self.assertIsNot(results[0].alignment, None)
+        self.assertEqual(results[0].query_start, 0)
+        self.assertEqual(results[0].query_end, 5)
+        self.assertEqual(results[0].target_start, 1)
+        self.assertEqual(results[0].target_end, 7)
+
+
+    
