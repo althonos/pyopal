564b656d40b1338f673bc7c4371417b593fc8542
Martin Larralde <martin.larralde@embl.de>
2026-03-08 00:02:26 +0100

    Fix `linetrace` being enabled by default in Cython sources

--- src/pyopal/lib.pyx
+++ src/pyopal/lib.pyx
@@ -1,5 +1,5 @@
 # distutils: language = c++
-# cython: language_level=3, linetrace=True, binding=True
+# cython: language_level=3, binding=True
 """Bindings to Opal, a SIMD-accelerated pairwise sequence aligner.
 
 References:
